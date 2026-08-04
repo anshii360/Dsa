7c22cebdbf84ad9e47abcf4a82389f099d9bd65e
anshii360 <anshiiiiii07@gmail.com>
2026-08-04 13:17:22 +0530

    Time: 3 ms (49.33%), Space: 32.4 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -4,7 +4,7 @@ public:
         vector<int>a;
         sort(nums.begin(),nums.end());
         for(int i=1;i<nums.size();i++){
-            if(nums[i]-nums[i-1]>1){
+            if(nums[i-1]+1!=nums[i]){
                 for(int x=nums[i-1]+1;x<nums[i];x++){
                 a.push_back(x);
                 }
